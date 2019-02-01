5fec75c44f9cac03f5fb3952fd6c36d6f1b1ee2d
Unknown <younes.ijichi@gmail.com>
2019-02-01 13:30:26 -0500

    tests termines

--- tp1/tests-hyp5.py
+++ tp1/tests-hyp5.py
@@ -1,7 +1,7 @@
 import codage_predictif_image as pred
 import codage_paires_octets_image as paires
 
-nomsImage = ['Image02hyp05.jpg', 'Image03hyp05.jpg']
+nomsImage = ['Image01hyp05.jpg', 'Image02hyp05.jpg', 'Image03hyp05.jpg']
 
 for nom in nomsImage:
     print('-----Image  :', nom)
