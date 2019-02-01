c81a6fd56a473277781bc3d3ec274bdcc7b63d60
Unknown <younes.ijichi@gmail.com>
2019-01-31 22:56:27 -0500

    redimension hypothese 3

--- tp1/tests.py
+++ tp1/tests.py
@@ -1,7 +1,9 @@
 import codage_predictif_image as pred
 import codage_paires_octets_image as paires
 
-nomsImage = ['Image01hyp01.jpg', 'Image01hyp03.jpg', 'Image02hyp01.jpg', 'Image02hyp03.jpg', 'Image03hyp01.jpg', 'Image03hyp03.jpg']
+nomsImage = ['Image01hyp01.jpg', 'Image02hyp01.jpg', 'Image03hyp01.jpg',
+             'Image01hyp03.jpg', 'Image02hyp03.jpg', 'Image03hyp03.jpg',
+             'Image01hyp05.jpg', 'Image02hyp05.jpg', 'Image03hyp05.jpg', ]
 
 for nom in nomsImage:
     print('Predictif  :')
