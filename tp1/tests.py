e661924dc3feef27c43bc9eb594517eb3413589b
Unknown <younes.ijichi@gmail.com>
2019-02-01 11:01:40 -0500

    reduction des images hyp 7 et hyp 3

--- tp1/tests.py
+++ tp1/tests.py
@@ -1,9 +1,9 @@
 import codage_predictif_image as pred
 import codage_paires_octets_image as paires
 
-nomsImage = ['Image01hyp03.jpg', 'Image02hyp03.jpg', 'Image03hyp03.jpg',
-             'Image01hyp05.jpg', 'Image02hyp05.jpg', 'Image03hyp05.jpg',
-             'Image01hyp07.jpg', 'Image02hyp07.jpg', 'Image03hyp07.jpg']
+nomsImage = ['Image01hyp05.jpg', 'Image02hyp05.jpg', 'Image03hyp05.jpg',
+             'Image01hyp07.jpg', 'Image02hyp07.jpg', 'Image03hyp07.jpg',
+             'Image01hyp03.jpg', 'Image02hyp03.jpg', 'Image03hyp03.jpg']
 
 for nom in nomsImage:
     print('-----Image  :', nom)
