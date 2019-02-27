60e70e7baf218747d47c5d8b0c3a9868b32347e1
Unknown <younes.ijichi@gmail.com>
2019-02-27 17:59:37 -0500

    tu penses

--- tp2/cbcr.py
+++ tp2/cbcr.py
@@ -4,14 +4,6 @@ import matplotlib.pyplot as py
 from PIL import Image
 
 # Etape 1 : Conversion RGB/YCBCR avec sous echantillonnage 4 : 2 : 0
-nomImage = "fjords.jpg"
-hauteur = 400
-largeur = 600
-
-fig1 = py.figure(figsize=(10, 10))
-image = py.imread(nomImage)
-# py.imshow(image)
-# py.show()
 
 """
 Y′= 0.299R+ 0.587G+ 0.114B
@@ -56,7 +48,7 @@ def getR(Y, Cr):
         return 255
     elif temp < 0:
         return 0
-    return temp
+    return int(temp)
 
 
 def getG(Y, Cr, Cb):
@@ -65,7 +57,7 @@ def getG(Y, Cr, Cb):
         return 255
     elif temp < 0:
         return 0
-    return temp
+    return int(temp)
 
 
 def getB(Y, Cb):
@@ -74,41 +66,79 @@ def getB(Y, Cb):
         return 255
     elif temp < 0:
         return 0
-    return temp
+    return int(temp)
 
+
+nomImage = "fjords.jpg"
+
+fig1 = py.figure(figsize=(10, 10))
+image = py.imread(nomImage)
+
+hauteur = len(image) - 1
+largeur = len(image[0]) - 1
+
+# py.imshow(image)
+# py.show()
 
 # Conversion 4:2:0
 code = []
 for ligne in range(hauteur):
     code.append([])
-    if ligne % 2 != 0:
-        for colonne in range(largeur):
-            pixel = image[ligne][colonne]
-            y = 0.299 * pixel[0] + 0.587 * pixel[1] + 0.114 * pixel[2]
+    code.append([])
+    for colonne in range(largeur):
+        # Case haut-gauche
+        pixel = image[ligne][colonne]
+        y = getY(pixel[0], pixel[1], pixel[2])
+        cb = getCb(pixel[1], y)
+        cr = getCr(pixel[0], y)
+        code[ligne].append([y, cb, cr])
+
+        # Case haut-droite
+        pixel = image[ligne][colonne + 1]
+        y = getY(pixel[0], pixel[1], pixel[2])
+        code[ligne].append([y, cb, cr])
+
+        # Case bas-gauche
+        pixel = image[ligne + 1][colonne]
+        y = getY(pixel[0], pixel[1], pixel[2])
+        code[ligne + 1].append([y, cb, cr])
+
+        # Case bas-droite
+        pixel = image[ligne + 1][colonne + 1]
+        y = getY(pixel[0], pixel[1], pixel[2])
+        code[ligne + 1].append([y, cb, cr])
+
+        # On saute une colonne
+        colonne += 1
+
+    # On saute une ligne
+    ligne += 1
+
+    """if ligne % 2 != 0:
+    for colonne in range(largeur):
+        pixel = image[ligne][colonne]
+        y = 0.299 * pixel[0] + 0.587 * pixel[1] + 0.114 * pixel[2]
+        code[ligne].append([y, 0, 0])
+else:
+    for colonne in range(largeur):
+        pixel = image[ligne][colonne]
+        y = 0.299 * pixel[0] + 0.587 * pixel[1] + 0.114 * pixel[2]
+        if colonne % 2 != 0:
             code[ligne].append([y, 0, 0])
-    else:
-        for colonne in range(largeur):
-            pixel = image[ligne][colonne]
-            y = 0.299 * pixel[0] + 0.587 * pixel[1] + 0.114 * pixel[2]
-            if colonne % 2 != 0:
-                code[ligne].append([y, 0, 0])
-            else:
-                cb = 128 + 0.564 * (pixel[2] - y)
-                cr = 128 + 0.713 * (pixel[0] - y)
-                code[ligne].append([y, cb, cr])
+        else:
+            cb = 128 + 0.564 * (pixel[2] - y)
+            cr = 128 + 0.713 * (pixel[0] - y)
+            code[ligne].append([y, cb, cr])"""
+
 
 # INVERSE : Conversion
 imageDecode = Image.new('RGB', (largeur, hauteur), "black")
 pixels = imageDecode.load()
 
-for colonne in range(largeur):
-    for ligne in range(hauteur):
+for ligne in range(hauteur):
+    for colonne in range(largeur):
         ycbcr = code[ligne][colonne]
-        r = ycbcr[0] + 1.403 * (ycbcr[2] - 128)
-        g = ycbcr[0] - 0.714 * (ycbcr[2] - 128) - 0.344 * (ycbcr[1] - 128)
-        b = ycbcr[0] + 1.773 * (ycbcr[1] - 128)
-        pixels[colonne, ligne] = (int(getR(ycbcr[0], ycbcr[2])), int(g), int(b))
+        pixels[colonne, ligne] = (getR(ycbcr[0], ycbcr[2]), getG(ycbcr[0], ycbcr[2], ycbcr[1]), getB(ycbcr[0], ycbcr[1]))
 
 py.imshow(imageDecode)
 py.show()
-#
