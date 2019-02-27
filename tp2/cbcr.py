ac70c9f624eee4fa1349c58cad02878f7701fdc5
Unknown <younes.ijichi@gmail.com>
2019-02-27 18:42:41 -0500

    420 en propageant Y

--- tp2/cbcr.py
+++ tp2/cbcr.py
@@ -3,25 +3,12 @@
 import matplotlib.pyplot as py
 from PIL import Image
 
-# Etape 1 : Conversion RGB/YCBCR avec sous echantillonnage 4 : 2 : 0
-
-"""
-Y′= 0.299R+ 0.587G+ 0.114B
-CB= 128 + 0.564(B−Y′)
-CR= 128 + 0.713(R−Y′)
-R=Y′+ 1.403(CR−128)
-G=Y′−0.714(CR−128)−0.344(CB−128)
-B=Y′+ 1.773(CB−128)
-"""
-
 
 def getY(R, G, B):
     temp = 0.299 * R + 0.587 * G + 0.114 * B
     if temp > 255:
-        print('255')
         return 255
     elif temp < 0:
-        print('0')
         return 0
     return temp
 
@@ -29,10 +16,8 @@ def getY(R, G, B):
 def getCb(B, Y):
     temp = 128 + 0.564 * (B - Y)
     if temp > 255:
-        print('255')
         return 255
     elif temp < 0:
-        print('0')
         return 0
     return temp
 
@@ -40,10 +25,8 @@ def getCb(B, Y):
 def getCr(R, Y):
     temp = 128 + 0.713 * (R - Y)
     if temp > 255:
-        print('255')
         return 255
     elif temp < 0:
-        print('0')
         return 0
     return temp
 
@@ -51,10 +34,8 @@ def getCr(R, Y):
 def getR(Y, Cr):
     temp = Y + 1.403 * (Cr - 128)
     if temp > 255:
-        print('255')
         return 255
     elif temp < 0:
-        print('0')
         return 0
     return int(temp)
 
@@ -62,10 +43,8 @@ def getR(Y, Cr):
 def getG(Y, Cr, Cb):
     temp = Y - 0.714 * (Cr - 128) - 0.344 * (Cb - 128)
     if temp > 255:
-        print('255')
         return 255
     elif temp < 0:
-        print('0')
         return 0
     return int(temp)
 
@@ -73,10 +52,8 @@ def getG(Y, Cr, Cb):
 def getB(Y, Cb):
     temp = Y + 1.773 * (Cb - 128)
     if temp > 255:
-        print('255')
         return 255
     elif temp < 0:
-        print('0')
         return 0
     return int(temp)
 
@@ -88,10 +65,10 @@ image = py.imread(nomImage)
 # py.imshow(image)
 # py.show()
 
-hauteur = len(image) - 1
-largeur = len(image[0]) - 1
+hauteur = len(image)
+largeur = len(image[0])
 
-# Conversion 4:4:4
+# Conversion RGB vers YCbCr 4:4:4
 code = image.copy()
 
 for ligne in range(hauteur):
@@ -102,7 +79,41 @@ for ligne in range(hauteur):
         code[ligne][colonne][1] = getCb(pixel[1], y)
         code[ligne][colonne][2] = getCr(pixel[0], y)
 
-# INVERSE : Conversion
+# Conversion YCbCr vers RGB
+imageDecode = image.copy()
+
+for ligne in range(hauteur):
+    for colonne in range(largeur):
+        ycbcr = code[ligne][colonne]
+        imageDecode[ligne][colonne] = (getR(ycbcr[0], ycbcr[2]),
+                                       getG(ycbcr[0], ycbcr[2], ycbcr[1]),
+                                       getB(ycbcr[0], ycbcr[1]))
+
+py.imshow(imageDecode)
+py.show()
+
+# Conversion RGB vers YCbCr 4:2:0
+code = image.copy()
+
+for ligne in range(hauteur):
+    if ligne % 2 == 0:
+        for colonne in range(largeur):
+            """ Si ligne impaire lire les infos au dessus de sa case sinon calculer ycbcr pour la case 
+            courante (si colonne impaire lire la case precedente)
+            On test avec un seul Y"""
+            if colonne % 2 == 0:
+                pixel = image[ligne][colonne]
+                y = getY(pixel[0], pixel[1], pixel[2])
+                code[ligne][colonne][0] = getY(pixel[0], pixel[1], pixel[2])
+                code[ligne][colonne][1] = getCb(pixel[1], y)
+                code[ligne][colonne][2] = getCr(pixel[0], y)
+            else:
+                code[ligne][colonne] = code[ligne][colonne - 1]
+    else:
+        # Copier la ligne au dessus
+        code[ligne] = code[ligne - 1]
+
+# Conversion YCbCr vers RGB
 imageDecode = image.copy()
 
 for ligne in range(hauteur):
