bd15a4e6873f6d5f3753fc4b4f8de5620b9e083a
Unknown <younes.ijichi@gmail.com>
2019-02-27 18:46:48 -0500

    functions ycbcr

--- tp2/cbcr.py
+++ tp2/cbcr.py
@@ -58,70 +58,77 @@ def getB(Y, Cb):
     return int(temp)
 
 
-nomImage = "fjords.jpg"
+def RGBtoYCbCr444(nomImage):
+    image = py.imread(nomImage)
+    # py.imshow(image)
+    # py.show()
 
-fig1 = py.figure(figsize=(10, 10))
-image = py.imread(nomImage)
-# py.imshow(image)
-# py.show()
+    hauteur = len(image)
+    largeur = len(image[0])
 
-hauteur = len(image)
-largeur = len(image[0])
+    # Conversion RGB vers YCbCr 4:4:4
+    code = image.copy()
 
-# Conversion RGB vers YCbCr 4:4:4
-code = image.copy()
-
-for ligne in range(hauteur):
-    for colonne in range(largeur):
-        pixel = image[ligne][colonne]
-        y = getY(pixel[0], pixel[1], pixel[2])
-        code[ligne][colonne][0] = getY(pixel[0], pixel[1], pixel[2])
-        code[ligne][colonne][1] = getCb(pixel[1], y)
-        code[ligne][colonne][2] = getCr(pixel[0], y)
-
-# Conversion YCbCr vers RGB
-imageDecode = image.copy()
-
-for ligne in range(hauteur):
-    for colonne in range(largeur):
-        ycbcr = code[ligne][colonne]
-        imageDecode[ligne][colonne] = (getR(ycbcr[0], ycbcr[2]),
-                                       getG(ycbcr[0], ycbcr[2], ycbcr[1]),
-                                       getB(ycbcr[0], ycbcr[1]))
-
-py.imshow(imageDecode)
-py.show()
-
-# Conversion RGB vers YCbCr 4:2:0
-code = image.copy()
-
-for ligne in range(hauteur):
-    if ligne % 2 == 0:
+    for ligne in range(hauteur):
         for colonne in range(largeur):
-            """ Si ligne impaire lire les infos au dessus de sa case sinon calculer ycbcr pour la case 
-            courante (si colonne impaire lire la case precedente)
-            On test avec un seul Y"""
-            if colonne % 2 == 0:
-                pixel = image[ligne][colonne]
-                y = getY(pixel[0], pixel[1], pixel[2])
-                code[ligne][colonne][0] = getY(pixel[0], pixel[1], pixel[2])
-                code[ligne][colonne][1] = getCb(pixel[1], y)
-                code[ligne][colonne][2] = getCr(pixel[0], y)
-            else:
-                code[ligne][colonne] = code[ligne][colonne - 1]
-    else:
-        # Copier la ligne au dessus
-        code[ligne] = code[ligne - 1]
+            pixel = image[ligne][colonne]
+            y = getY(pixel[0], pixel[1], pixel[2])
+            code[ligne][colonne][0] = getY(pixel[0], pixel[1], pixel[2])
+            code[ligne][colonne][1] = getCb(pixel[1], y)
+            code[ligne][colonne][2] = getCr(pixel[0], y)
 
-# Conversion YCbCr vers RGB
-imageDecode = image.copy()
+    # Conversion YCbCr vers RGB
+    imageDecode = image.copy()
 
-for ligne in range(hauteur):
-    for colonne in range(largeur):
-        ycbcr = code[ligne][colonne]
-        imageDecode[ligne][colonne] = (getR(ycbcr[0], ycbcr[2]),
-                                       getG(ycbcr[0], ycbcr[2], ycbcr[1]),
-                                       getB(ycbcr[0], ycbcr[1]))
+    for ligne in range(hauteur):
+        for colonne in range(largeur):
+            ycbcr = code[ligne][colonne]
+            imageDecode[ligne][colonne] = (getR(ycbcr[0], ycbcr[2]),
+                                           getG(ycbcr[0], ycbcr[2], ycbcr[1]),
+                                           getB(ycbcr[0], ycbcr[1]))
 
-py.imshow(imageDecode)
-py.show()
+    py.imshow(imageDecode)
+    py.show()
+
+
+def RGBtoYCbCr420(nomImage):
+    image = py.imread(nomImage)
+    hauteur = len(image)
+    largeur = len(image[0])
+    code = image.copy()
+
+    for ligne in range(hauteur):
+        if ligne % 2 == 0:
+            for colonne in range(largeur):
+                """ Si ligne impaire lire les infos au dessus de sa case sinon calculer ycbcr pour la case 
+                courante (si colonne impaire lire la case precedente)
+                On test avec un seul Y"""
+                if colonne % 2 == 0:
+                    pixel = image[ligne][colonne]
+                    y = getY(pixel[0], pixel[1], pixel[2])
+                    code[ligne][colonne][0] = getY(pixel[0], pixel[1], pixel[2])
+                    code[ligne][colonne][1] = getCb(pixel[1], y)
+                    code[ligne][colonne][2] = getCr(pixel[0], y)
+                else:
+                    code[ligne][colonne] = code[ligne][colonne - 1]
+        else:
+            # Copier la ligne au dessus
+            code[ligne] = code[ligne - 1]
+
+    # Conversion YCbCr vers RGB
+    imageDecode = image.copy()
+
+    for ligne in range(hauteur):
+        for colonne in range(largeur):
+            ycbcr = code[ligne][colonne]
+            imageDecode[ligne][colonne] = (getR(ycbcr[0], ycbcr[2]),
+                                           getG(ycbcr[0], ycbcr[2], ycbcr[1]),
+                                           getB(ycbcr[0], ycbcr[1]))
+
+    py.imshow(imageDecode)
+    py.show()
+
+
+# Main :
+nomImage = "fjords.jpg"
+RGBtoYCbCr420(nomImage)
