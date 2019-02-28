c94e62841f2c4f1fdcf69d52dff89d18aeb4193d
Unknown <younes.ijichi@gmail.com>
2019-02-28 18:44:31 -0500

    huffman et par plages

--- tp2/main.py
+++ tp2/main.py
@@ -3,20 +3,22 @@
 import ycbcr_converter as ycbcr
 import bloc_divider as bdiv
 import matplotlib.pyplot as py
+import huffman
+import parplages
 
 image = py.imread('fjords.jpg').astype('float')
 
 code420 = ycbcr.RGBtoYCbCr420(image)
 decode420 = ycbcr.YCbCr420toRGB(code420)
-py.imshow(decode420)
-py.show()
+# py.imshow(decode420)
+# py.show()
 
 code444 = ycbcr.RGBtoYCbCr444(image)
 decode444 = ycbcr.YCbCr444toRGB(code444)
-py.imshow(decode444)
-py.show()
+# py.imshow(decode444)
+# py.show()
 
 blocs = bdiv.divider8by8(image)
 imageReconstruite = bdiv.rebuildFrom8by8(blocs)
-py.imshow(imageReconstruite)
-py.show()
+# py.imshow(imageReconstruite)
+# py.show()
