66f37f3afcbc6f5700217823896a7368135036c0
darthann <younes.ijichi@gmail.com>
2019-03-01 14:23:36 -0500

    QA et source zigzag

--- tp2/zigzag.py
+++ tp2/zigzag.py
@@ -1,11 +1,13 @@
+# -*- coding: utf-8 -*-
+# https://github.com/wzq94qzw/naive-jpeg
+
 import matplotlib.pyplot as py
 import numpy as np
-image = py.imread('C:/Users/USER/Desktop/INF8770_TP2/inf8770/tp2/fjords.jpg').astype('float')
+
 
 def zig_zag(input_matrix,block_size):
     z = np.empty([block_size*block_size*3])
     index = -1
-    bound = 0
     for i in range(0, 2 * block_size -1):
         if i < block_size:
             bound = 0
@@ -27,10 +29,11 @@ def zig_zag(input_matrix,block_size):
                 z[index] = input_matrix[i - j, j][2]
     return z
 
+
 def zig_zag_reverse(input_matrix,block_size):
+    image = py.imread('fjords.jpg').astype('float')
     output_matrix = np.empty([image.shape[0],image.shape[1],3])
     index = -1
-    bound = 0
     for i in range(0, 2 * block_size -1):
         if i < block_size:
             bound = 0
