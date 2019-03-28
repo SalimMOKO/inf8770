a9b3531d0327af723de014ebefa280cd291fa2b6
Unknown <younes.ijichi@gmail.com>
2019-03-28 12:36:50 -0400

    frame precedente

--- tp3/main.py
+++ tp3/main.py
@@ -4,11 +4,22 @@ import matplotlib.pyplot as py
 
 capture = cv2.VideoCapture("julia.avi")
 
-for i in range(2):
-    ret, frame = capture.read()
-    image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    py.imshow(image)
-    py.show()
+ret, frame = capture.read()
+index = 0
 
+# First frame treatment
+framePrec = frame
+index += 1
+
+while ret:
+    if frame is not None:
+        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    # py.imshow(image)
+    # py.show()
+    framePrec = frame
+    index += 1
+    ret, frame = capture.read()
+
+print("End of video at ", index)
 capture.release()
 cv2.destroyAllWindows()
